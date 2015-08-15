bf9159ca7a11d4030d5f670733f74d060e126dad
Akilgour <kilgourkilgour@hotmail.com>
2015-08-15 12:52:51 +0100

    Created the animal object

---
+++ TableDrivenDevelopment/Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TableDrivenDevelopment.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Legs { get; set; }
+        public int Wings { get; set; }
+        public bool CanGoUnderwater { get; set; }
+        public bool CanFly { get; set; }
+        public string Voice { get; set; }
+    }
+}
