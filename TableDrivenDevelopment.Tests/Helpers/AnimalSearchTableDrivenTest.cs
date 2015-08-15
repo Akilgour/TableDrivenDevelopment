6802a0270520580845ddb55fa0fac61010479da0
Akilgour <kilgourkilgour@hotmail.com>
2015-08-15 18:35:45 +0100

    Created search first pass and stub of unit test

---
+++ TableDrivenDevelopment.Tests/Helpers/AnimalSearchTableDrivenTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableDrivenDevelopment.Tests.Factories;
+using TableDrivenDevelopment.Helpers;
+using TableDrivenDevelopment.Models;
+
+namespace TableDrivenDevelopment.Tests.Helpers
+{
+    [TestClass]
+    public class AnimalSearchTableDrivenTest
+    {
+        [TestMethod]
+        public void GetCatByNameCount()
+        {
+            //Arrange 
+            var animalList = FourAnimalFactory.GetAnimals();
+
+            var searchAnimal = CatFactory.GetCat();
+
+            //Act 
+            var resultsAnimals = AnimalSearchTableDriven.Search(animalList, searchAnimal);
+
+            //Assert
+            Assert.AreEqual(resultsAnimals.Count, 1);
+        }
+
+
+    }
+}
---
+++ TableDrivenDevelopment/Helpers/AnimalSearchTableDriven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TableDrivenDevelopment.Models;
+
+namespace TableDrivenDevelopment.Helpers
+{
+    public static class AnimalSearchTableDriven
+    {
+
+        public static List<Animal> Search(List<Animal> animalList, Animal seachAnimal)
+        {
+            var list = new List<Animal>();
+            list.Add(animalList.First());
+            return list;
+        }
+
+    }
+}
