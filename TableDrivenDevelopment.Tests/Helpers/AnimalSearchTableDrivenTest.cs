eaf241d512b01588c9c71cf062e743150d1ca01a
Akilgour <kilgourkilgour@hotmail.com>
2015-08-15 20:43:44 +0100

    Got search to work by name

--- TableDrivenDevelopment.Tests/Helpers/AnimalSearchTableDrivenTest.cs
+++ TableDrivenDevelopment.Tests/Helpers/AnimalSearchTableDrivenTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TableDrivenDevelopment.Tests.Factories;
 using TableDrivenDevelopment.Helpers;
 using TableDrivenDevelopment.Models;
+using System.Collections.Generic;
 
 namespace TableDrivenDevelopment.Tests.Helpers
 {
@@ -10,7 +11,7 @@ namespace TableDrivenDevelopment.Tests.Helpers
     public class AnimalSearchTableDrivenTest
     {
         [TestMethod]
-        public void GetCatByNameCount()
+        public void GetCatCount()
         {
             //Arrange 
             var animalList = FourAnimalFactory.GetAnimals();
@@ -24,6 +25,55 @@ namespace TableDrivenDevelopment.Tests.Helpers
             Assert.AreEqual(resultsAnimals.Count, 1);
         }
 
+        [TestMethod]
+        public void GetCatByName()
+        {
+            //Arrange 
+            var animalList = FourAnimalFactory.GetAnimals();
+            var searchAnimal = new Animal()
+            {
+                Name = "Cat"
+            };
 
+            //Act 
+            List<Animal> resultsAnimals = AnimalSearchTableDriven.Search(animalList, searchAnimal);
+
+            //Assert
+            Assert.AreEqual(resultsAnimals[0].Name, "Cat");
+        }
+
+        [TestMethod]
+        public void GetCountNoneWhereNameNull()
+        {
+            //Arrange 
+            var animalList = FourAnimalFactory.GetAnimals();
+            var searchAnimal = new Animal()
+            {
+                Name = null
+            };
+
+            //Act 
+            List<Animal> resultsAnimals = AnimalSearchTableDriven.Search(animalList, searchAnimal);
+
+            //Assert
+            Assert.AreEqual(resultsAnimals.Count, 0);
+        }
+
+        [TestMethod]
+        public void GetCountNoneWhereNameByStringEmpty()
+        {
+            //Arrange 
+            var animalList = FourAnimalFactory.GetAnimals();
+            var searchAnimal = new Animal()
+            {
+                Name = String.Empty
+            };
+
+            //Act 
+            List<Animal> resultsAnimals = AnimalSearchTableDriven.Search(animalList, searchAnimal);
+
+            //Assert
+            Assert.AreEqual(resultsAnimals.Count, 0);
+        }
     }
 }
--- TableDrivenDevelopment/Helpers/AnimalSearchTableDriven.cs
+++ TableDrivenDevelopment/Helpers/AnimalSearchTableDriven.cs
@@ -11,9 +11,32 @@ namespace TableDrivenDevelopment.Helpers
 
         public static List<Animal> Search(List<Animal> animalList, Animal seachAnimal)
         {
-            var list = new List<Animal>();
-            list.Add(animalList.First());
-            return list;
+            //var list = new List<Animal>();
+            //list.Add(animalList.First());
+            //return list;
+
+            //IEnumerable<Animal> users = from user in animalList
+            //                          where
+            //                               (seachAnimal.ScreenName != null && user.ScreenName.Contains(search))
+            //                               || (user.FirstName != null && user.FirstName.Contains(search))
+            //                               || (user.LastName != null && user.LastName.Contains(search))
+            //                          select user;
+
+            //var users = from user in animalList
+            //            where
+            //                 (user.Name != null && user.Name.Contains(seachAnimal.Name))
+            //            select user; 
+
+            //var users = from user in animalList
+            //            where
+            //                 ((user.Name != null && seachAnimal.Name != null && seachAnimal.Name != string.Empty ) && user.Name.Contains(seachAnimal.Name))
+            //            select user;        
+
+            var resultsAnimal = from animal in animalList
+                                where ((animal.Name != null && seachAnimal.Name != null && seachAnimal.Name != string.Empty) && animal.Name.Contains(seachAnimal.Name))
+                                select animal;
+            return resultsAnimal.ToList();
+
         }
 
     }
