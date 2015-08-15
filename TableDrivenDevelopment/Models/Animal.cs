c6c12cbd0c7b04fabed7671dacb73d12ddb74526
Akilgour <kilgourkilgour@hotmail.com>
2015-08-15 16:08:50 +0100

    Added an Id and changed some varables names

--- TableDrivenDevelopment/Models/Animal.cs
+++ TableDrivenDevelopment/Models/Animal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,11 @@ namespace TableDrivenDevelopment.Models
 {
     public class Animal
     {
+        [Key]
+        public Guid Id { get; set; }
         public string Name { get; set; }
-        public int Legs { get; set; }
-        public int Wings { get; set; }
+        public int NumberOfLegs { get; set; }
+        public int NumberOfWings { get; set; }
         public bool CanGoUnderwater { get; set; }
         public bool CanFly { get; set; }
         public string Voice { get; set; }
